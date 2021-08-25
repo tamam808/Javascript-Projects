ffb49dfd73ad7f0f425ddec8f6ac0fc790e1b707
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-08-25 15:44:13 -0600

    learning and implementing basic JS code

---
+++ Basic-Javascript-Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,33 @@
+document.write('Hello, World! '); //Printing Hello World
+
+var A = "This is a string " ;     //defining variable with string value  
+
+document.write(A) ;               //printing variable
+
+var P = "This is an alert" ; //defing variable with string
+
+window.alert(P) ; //alerting with variable 
+
+document.write("\"I\'m gonna put some dirt in your eye,\" said Peter Parker. ") ; //practicing using \ to include ' and " in texts
+
+document.write("Sometimes you just gotta be epic and sometimes" // concatenating text using +
++ " you gotta know when to walk away. ") ;
+
+var C = "This is my concatenated string. It\'s just going to be a"          //defining a variable with text that includes ' and concatenating text
++ " super long run on sentence that really doesn\'t go anywhere but oh"
++ " well it\'s still valid in my opinion. " ;
+document.write(C) ; //printing variable
+
+var firstname = "Tama", age = 26, icecream = "Jamoca Almond Fudge", favanime = "Naruto" ; //defining multiple variables in 1 line of code
+var firstname = firstname.fontcolor("red"); //assigning font color to a variable
+document.write(firstname + "."); //printing colored variable
+
+var blues = " I have the blues."; //defining variable
+var blues = blues.fontcolor("blue"); //assigning font color to variable
+document.write(blues); //printing colored variable
+
+document.write(3 + 4); //expression using math
+
+var J = "This is my "; //defining variable with string
+var K = "\"concatenated variables\" assignment." //defining variable with string
+document.write(J + K); //concatenating variables and printing them
