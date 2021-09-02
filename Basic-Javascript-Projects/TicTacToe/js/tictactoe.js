6ad68e13d0f538c7782c321918c634066434cad7
tamam808 <70548075+tamam808@users.noreply.github.com>
2021-09-02 15:24:58 -0600

    fixed error in code on lines 42 and 50

--- Basic-Javascript-Projects/TicTacToe/js/tictactoe.js
+++ Basic-Javascript-Projects/TicTacToe/js/tictactoe.js
@@ -39,7 +39,7 @@ function placeXorO(squareNumber) { //This function if for placing x or o in a sq
 function checkWinConditions() { //This function parses the selectedSquares array to search for win conditions. drawWinLine function is called to draw line if condition is met
     if      (arrayIncludes('0X', '1X', '2X')) { drawWinLine(50,100,558,100) }   //X 0,1,2 condition
     else if (arrayIncludes('3X', '4X', '5X')) { drawWinLine(50,304,558,304) }   //X 3,4,5 condition
-    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50,508,558,304) }   //X 6,7,8 condition
+    else if (arrayIncludes('6X', '7X', '8X')) { drawWinLine(50,508,558,508) }   //X 6,7,8 condition
     else if (arrayIncludes('0X', '3X', '6X')) { drawWinLine(100,50,100,558) }   //X 0,3,6 condition
     else if (arrayIncludes('1X', '4X', '7X')) { drawWinLine(304,50,304,558) }   //X 1,4,7 condition
     else if (arrayIncludes('2X', '5X', '8X')) { drawWinLine(508,50,508,558) }   //X 2,5,8 condition
@@ -47,7 +47,7 @@ function checkWinConditions() { //This function parses the selectedSquares array
     else if (arrayIncludes('0X', '4X', '8X')) { drawWinLine(100,100,520,520) }  //X 0,4,8 condition
     else if (arrayIncludes('0O', '1O', '2O')) { drawWinLine(50,100,558,100) }   //O 0,1,2 condition
     else if (arrayIncludes('3O', '4O', '5O')) { drawWinLine(50,304,558,304) }   //O 3,4,5 condition
-    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(100,50,100,508) }   //O 6,7,8 condition
+    else if (arrayIncludes('6O', '7O', '8O')) { drawWinLine(50,508,558,508) }   //O 6,7,8 condition
     else if (arrayIncludes('0O', '3O', '6O')) { drawWinLine(100,50,100,558) }   //O 0,3,6 condition
     else if (arrayIncludes('1O', '4O', '7O')) { drawWinLine(304,50,304,558) }   //O 1,4,7 condition
     else if (arrayIncludes('2O', '5O', '8O')) { drawWinLine(508,50,508,558) }   //O 2,5,8 condition
